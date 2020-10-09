837f61ced5305d6422fd9831872e1cef94a3d4d1
李绍丰 <47840575+Crazydear@users.noreply.github.com>
2020-10-09 17:29:35 +0800

    Update 04-fan.sh

--- Scripts/00-09/04-fan.sh
+++ Scripts/00-09/04-fan.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # 利用crontab定时，每隔一分钟运行一次
 
-if [ -e /sys/class/gpio/gpio14/direction ]
+if [ -e /sys/class/gpio/gpio26/direction ]
 then
 echo "已经挂载"
 else
 echo "未挂载"
-echo 14 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio14/direction
-echo 1 > /sys/class/gpio/gpio14/value
+echo 26 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio26/direction
+echo 1 > /sys/class/gpio/gpio26/value
 fi
 
 tem=$(cat /sys/class/thermal/thermal_zone0/temp)
@@ -16,12 +16,12 @@ echo $tem
 
 if [ $tem -ge 65000 ]
 then
-echo out > /sys/class/gpio/gpio14/direction
+echo out > /sys/class/gpio/gpio26/direction
 fi
 
 if [ $tem -le 40000 ]
 then
-echo 1 > /sys/class/gpio/gpio14/value 
+echo 1 > /sys/class/gpio/gpio26/value 
 # echo 14 >/sys/class/gpio/unexport
 echo "温度不高"
 fi
