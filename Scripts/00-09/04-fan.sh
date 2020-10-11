6ce493aa026ad24a093a2898e447e7a44a1e81f6
李绍丰 <47840575+Crazydear@users.noreply.github.com>
2020-10-11 14:41:09 +0800

    Update 04-fan.sh

--- Scripts/00-09/04-fan.sh
+++ Scripts/00-09/04-fan.sh
@@ -8,7 +8,7 @@ else
 echo "未挂载"
 echo 26 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio26/direction
-echo 1 > /sys/class/gpio/gpio26/value
+
 fi
 
 tem=$(cat /sys/class/thermal/thermal_zone0/temp)
@@ -17,11 +17,11 @@ echo $tem
 if [ $tem -ge 65000 ]
 then
 echo out > /sys/class/gpio/gpio26/direction
+echo 0 > /sys/class/gpio/gpio26/value
 fi
 
 if [ $tem -le 40000 ]
 then
 echo 1 > /sys/class/gpio/gpio26/value 
-# echo 14 >/sys/class/gpio/unexport
 echo "温度不高"
 fi
